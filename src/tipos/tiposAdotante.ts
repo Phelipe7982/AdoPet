a65e9bf9b35722da7bf5a75d57e824c2b8c076ed
Phelps <ultimaterafa79@gmail.com>
2024-12-22 21:07:15 -0300

    my first commit

---
+++ src/tipos/tiposAdotante.ts
@@ -0,0 +1,11 @@
+import AdotanteEntity from "../entities/AdotanteEntity";
+
+type TipoRequestBodyAdotante = Omit<AdotanteEntity, "id" | "pets">;
+
+type TipoRequestParamsAdotante = { id?: string };
+
+type TipoResponseBodyAdotante = {
+  dados?: | Pick<AdotanteEntity, "id" | "nome" | "celular" | "endereco"> | Pick<AdotanteEntity, "id" | "nome" | "celular" | "endereco">[];
+};
+
+export { TipoRequestBodyAdotante, TipoResponseBodyAdotante, TipoRequestParamsAdotante };
